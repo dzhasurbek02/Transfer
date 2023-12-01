038226a692f5d5555e43edde2b8f00f21b6980d0
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-12-01 08:43:10 +0500

    context updated

--- Transfer/Context/ApplicationDbContext.cs
+++ Transfer/Context/ApplicationDbContext.cs
@@ -6,6 +6,8 @@ namespace Transfer.Context;
 
 public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+    
     public DbSet<User> Users { get; set; }
     
     public DbSet<PaymentMethod> PaymentMethods { get; set; }
