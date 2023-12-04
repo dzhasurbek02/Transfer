86260a3020d5e81bb94065c4cf6b267d3264acf4
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-12-04 16:38:58 +0500

    CreateUserValidator.cs updated

--- Transfer/Features/User/CreateUser/CreateUserValidator.cs
+++ Transfer/Features/User/CreateUser/CreateUserValidator.cs
@@ -11,21 +11,21 @@ public class CreateUserValidator : AbstractValidator<CreateUserRequest>
             .NotEmpty()
             .WithMessage("Поле LastName не должно быть пустым!")
             .Length(2, 50)
-            .WithMessage("Длина LastName не должна быть меньше 2 символов и больше 50 символов!");;
+            .WithMessage("Длина LastName не должна быть меньше 2 символов и больше 50 символов!");
 
         RuleFor(u => u.FirstName)
             .Cascade(CascadeMode.Stop)
             .NotEmpty()
             .WithMessage("Поле FirstName не должно быть пустым!")
             .Length(2, 50)
-            .WithMessage("Длина FirstName не должна быть меньше 2 символов и больше 50 символов!");;
+            .WithMessage("Длина FirstName не должна быть меньше 2 символов и больше 50 символов!");
 
         RuleFor(u => u.PhoneNumber)
             .Cascade(CascadeMode.Stop)
             .NotEmpty()
             .WithMessage("Поле PhoneNumber не должно быть пустым!")
             .Length(11, 20)
-            .WithMessage("Длина PhoneNumber не должна быть меньше 11 чисел и больше 20 чисел!");;
+            .WithMessage("Длина PhoneNumber не должна быть меньше 11 чисел и больше 20 чисел!");
 
         RuleFor(u => u.Email)
             .Cascade(CascadeMode.Stop)
